d2b8fd0daf1cc1d85c3774732258b3bf657f96d4
7amdy22 <Muhamedhamdy22@example.com>
2026-01-19 16:58:28 -0800

    assets added

--- lib/screens/onboarding_screens/onboarding_screens.dart
+++ lib/screens/onboarding_screens/onboarding_screens.dart
@@ -6,6 +6,10 @@ class OnboardingScreens extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Image.asset("assets/images/logo.png"),
+      ),
+    );
   }
 }
