f004002f375772e744b76ae2581559976cdde203
7amdy22 <Muhamedhamdy22@example.com>
2026-01-19 16:38:36 -0800

    init

---
+++ lib/screens/onboarding_screens/onboarding_screens.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class OnboardingScreens extends StatelessWidget {
+  static const String routeName = "onboarding screens";
+   OnboardingScreens({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
