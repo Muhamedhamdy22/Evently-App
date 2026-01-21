f40c98ab9e39f6959a4dd36704a873e39e1195d5
7amdy22 <Muhamedhamdy22@example.com>
2026-01-21 14:47:03 -0800

    localization & theme almost done

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:evently_app/screens/onboarding_screens/onboarding_screens.dart';
 import 'package:flutter/material.dart';
 
+import 'core/my_theme.dart';
 import 'screens/home_screen/home_screen.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+
+  runApp( EasyLocalization(
+      supportedLocales: [Locale('en', 'US'), Locale('ar', 'EG')],
+      path: 'assets/translations',
+      fallbackLocale: Locale('en', 'US'),
+      child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -12,7 +21,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
       debugShowCheckedModeBanner: false,
+      theme: MyThemeData.lightTheme,
+      darkTheme: MyThemeData.darkTheme,
+      themeMode: ThemeMode.light,
       initialRoute: OnboardingScreens.routeName,
       routes: {
         OnboardingScreens.routeName : (context) => OnboardingScreens(),
